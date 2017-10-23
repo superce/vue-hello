f4e87248bb141b415136495a29b28c7d8d62be44
superce <562923778@qq.com>
2017-10-23 17:32:16 +0000

    vue

---
+++ src/components/CommentBox.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="commentBox">
+    <input type="text" id="commentForm" @keyup.enter="submitComment" >
+    <button id='btn' @click="submitComment">提交</button>
+    <ul>
+      <li v-for="comment in comments">
+        {{comment.text}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'commentBox',
+    data: () => ({
+      comments: [
+        { text: 'Foo' },
+        { text: 'Bar' }
+      ]
+    }),
+    methods: {
+      submitComment () {
+        let input = document.getElementById('commentForm')
+        if (input.value !== '') {
+          this.comments.push({text: input.value})
+          input.value = ''
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.commentBox{
+  height: 150px;
+  width: 80%;
+  margin: 0 auto;
+  background-color: #fff;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.5);
+  margin-bottom: 30px;
+}
+.commentBox ul{
+  list-style: none;
+}
+.commentBox li{
+  font-size: 18px;
+  line-height: 25px;
+}
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="post">
+    <div class="upper">
+      <post-body></post-body>
+    </div>
+    <div class="bottom">
+      <CommentBox></CommentBox>
+    </div>
+  </div>
+</template>
+
+<script>
+  import PostBody from './PostBody'
+  import CommentBox from './CommentBox'
+  export default {
+    components: {PostBody, CommentBox}
+  }
+</script>
+
+<style scped>
+  .post{
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  .upper{
+    background-color: #00bcd4;
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+  .bottom{
+    background-color: #f4f3f3;
+    flex-grow: 1;
+    padding-top: 40px;
+  }
+</style>
